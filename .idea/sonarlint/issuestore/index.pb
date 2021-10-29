648e32beaff74559b8728f02190995c8d5b11480
Iván Coello Cortés <ivancoellocortes@gmail.com>
2021-10-29 20:05:50 +0200

    modificaciones en el ejercicio 1

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+g
+7Python/01_EjerciciosBasicos/01_AreasPerimetros/areas.py,a\8\a86087ac1aa25c6e4dd2d21325bf424788f4bde7
--- Python/01_EjerciciosBasicos/01_AreasPerimetros/areas.py
+++ Python/01_EjerciciosBasicos/01_AreasPerimetros/areas.py
@@ -2,12 +2,12 @@ import math
 
 
 def area_cuadrado(l):
-    return l**2
+    return l ** 2
 
 
 def area_cubo(l):
-    return 6 * (l**2)
+    return 6 * (l ** 2)
 
 
 def area_circulo(r):
-    return math.pi * r**2
+    return math.pi * r ** 2
