989cbe33294066e14013d0113b136b5dc8c5387b
Iván Coello Cortés <ivancoellocortes@gmail.com>
2021-10-29 14:42:52 +0200

    Se ha implementado el ejercicio de los perímetros y áreas

---
+++ Python/01_EjerciciosBasicos/01_AreasPerimetros/areas.py
@@ -0,0 +1,13 @@
+import math
+
+
+def area_cuadrado(l):
+    return l**2
+
+
+def area_cubo(l):
+    return 6 * (l**2)
+
+
+def area_circulo(r):
+    return math.pi * r**2
---
+++ Python/01_EjerciciosBasicos/01_AreasPerimetros/perimetros.py
@@ -0,0 +1,13 @@
+import math
+
+
+def perimetro_cuadrado(l):
+    return l * 4
+
+
+def perimetro_cubo(l):
+    return 12 * l
+
+
+def perimetro_circulo(r):
+    return 2 * math.pi * r
