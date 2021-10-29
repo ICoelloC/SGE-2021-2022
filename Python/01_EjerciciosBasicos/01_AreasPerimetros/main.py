9447090e5e7d3a9d76861682811d0df292b8aef7
Iván Coello Cortés <ivancoellocortes@gmail.com>
2021-10-29 14:45:43 +0200

    Modificada la esrtuctura del ejercicio 1

--- Python/01_EjerciciosBasicos/01_AreasPerimetros/main.py
+++ Python/01_EjerciciosBasicos/01_AreasPerimetros/main.py
@@ -2,11 +2,12 @@ import areas
 import perimetros
 import utilidades
 
+
 def menu_principal():
     salir = False
 
     while not salir:
-        
+
         print('Ejercicio 1.\n')
         print('1.Calcular perímetros.\n2.Calcular áreas.\n3.Salir')
         opc = utilidades.pedir_entero('Escoja la opción: ')
@@ -18,9 +19,9 @@ def menu_principal():
         elif opc == 2:
             menu_areas(salir)
 
-
         elif opc == 3:
             salir = True
+
 
 def menu_areas(salir):
     while not salir:
@@ -38,11 +39,12 @@ def menu_areas(salir):
 
         elif x == 3:
             R = utilidades.pedir_entero(
-                        'Ingrese el radio de la circunferencia: ')
+                'Ingrese el radio de la circunferencia: ')
             print('el area del círculo es %.3f ' % areas.area_circulo(R))
 
         elif x == 4:
             salir = True
+
 
 def menu_perimetros(salir):
     while not salir:
@@ -53,20 +55,21 @@ def menu_perimetros(salir):
         if x == 1:
             L = utilidades.pedir_entero('Ingrese la longitud del lado: ')
             print('el perímetro del cuadrado es: ',
-                        perimetros.perimetro_cuadrado(L))
+                  perimetros.perimetro_cuadrado(L))
 
         elif x == 2:
             L = utilidades.pedir_entero('Ingrese la longitud de la cara: ')
             print('el perímetro del cubo es: %.3f ' %
-                        perimetros.perimetro_cubo(L))
+                  perimetros.perimetro_cubo(L))
 
         elif x == 3:
             R = utilidades.pedir_entero(
-                        'Ingrese el radio de la circunferencia: ')
+                'Ingrese el radio de la circunferencia: ')
             print('el perímetro de la circunferencia es %.3f ' %
-                        perimetros.perimetro_circulo(R))
+                  perimetros.perimetro_circulo(R))
 
         elif x == 4:
             salir = True
+
 
 menu_principal()
