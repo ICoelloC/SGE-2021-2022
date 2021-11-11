b35cee78be76e9da7070f55343ca9cbb8c66f368
Iván Coello Cortés <ivancoellocortes@gmail.com>
2021-11-11 10:15:42 +0100

    Añadidos los dos primeros ejercicios de secuencias

---
+++ Python/02_Secuencias/02_GeneradorNumerosPrimos/main.py
@@ -0,0 +1,6 @@
+import utilidades
+
+v_max = utilidades.pedir_entero(
+    "Introduce el rango maximo para generar numeros primos: ")
+lista = utilidades.generador_primos(v_max)
+print(lista)
---
+++ Python/02_Secuencias/02_GeneradorNumerosPrimos/utilidades.py
@@ -0,0 +1,24 @@
+def pedir_entero(mensaje):
+    correcto = False
+    num = 0
+    while(not correcto):
+        try:
+            num = int(input(mensaje))
+            correcto = True
+        except ValueError:
+            print('Error, introduce un numero entero')
+
+    return num
+
+
+def es_primo(numero):
+    if numero < 1:
+        return False
+    elif numero == 2:
+        return True
+    else:
+        return all(numero % i != 0 for i in range(2, numero))
+
+
+def generador_primos(maximo):
+    return [n for n in range(maximo) if es_primo(n)]
