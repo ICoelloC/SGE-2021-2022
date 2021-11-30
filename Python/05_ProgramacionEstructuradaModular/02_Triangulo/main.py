e3f09aa0fdb1fe97dde1f422dd62d14bd12d1993
Iván Coello Cortés <ivancoellocortes@gmail.com>
2021-11-30 18:08:02 +0100

    Añadido otro ejemplo

---
+++ Python/05_ProgramacionEstructuradaModular/02_Triangulo/main.py
@@ -0,0 +1,23 @@
+import utilidades as ut
+
+
+def menu_principal(lst_triangulos):
+    print("""
+    1. Crear Triángulo
+    2. Listar Triángulos
+    3. Salir
+    """)
+    opcion = ut.pedir_entero("Ingrese una opción: ")
+
+    if opcion == 1:
+        lst_triangulos.append(ut.crear_triangulo())
+    elif opcion == 2:
+        ut.listar_triangulos(lst_triangulos)
+    elif opcion == 3:
+        print("Saliendo...")
+    else:
+        print("Opción inválida")
+
+
+triangulos = []
+menu_principal(triangulos)
